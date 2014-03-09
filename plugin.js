ab6c636dcf9fa480a66186a44423b16b278e552c
Alfonso Martínez de Lizarrondo <amla70@gmail.com>
2014-03-09 22:28:03 +0100

    version 1.5, updated compatibility and bgcolor

--- plugin.js
+++ plugin.js
@@ -8,13 +8,13 @@
  * choice:
  *
  *  - GNU General Public License Version 2 or later (the "GPL")
- *    http://www.gnu.org/licenses/gpl.html
+ *	http://www.gnu.org/licenses/gpl.html
  *
  *  - GNU Lesser General Public License Version 2.1 or later (the "LGPL")
- *    http://www.gnu.org/licenses/lgpl.html
+ *	http://www.gnu.org/licenses/lgpl.html
  *
  *  - Mozilla Public License Version 1.1 or later (the "MPL")
- *    http://www.mozilla.org/MPL/MPL-1.1.html
+ *	http://www.mozilla.org/MPL/MPL-1.1.html
  *
  * == END LICENSE ==
  *
@@ -38,17 +38,105 @@ CKEDITOR.plugins.add( 'backgrounds',
 				allowedContent: {
 					'table td th': {
 						propertiesOnly: true,
-                        attributes: 'background',
-                        styles: 'background-repeat,background-position'
+						attributes: 'background',
+						styles: 'background-repeat,background-position'
 					}
 				}
 			} );
+		}
+
+		// For compatibility with older versions of CKEditor
+		if (!editor.getColorFromDialog)
+		{
+			/**
+			 * Open up color dialog and to receive the selected color.
+			 *
+			 * @param {Function} callback The callback when color dialog is closed
+			 * @param {String} callback.color The color value received if selected on the dialog.
+			 * @param [scope] The scope in which the callback will be bound.
+			 * @member CKEDITOR.editor
+			 */
+			editor.getColorFromDialog = function( callback, scope ) {
+				var onClose = function( evt ) {
+					releaseHandlers( this );
+					var color = evt.name == 'ok' ? this.getValueOf( 'picker', 'selectedColor' ) : null;
+					callback.call( scope, color );
+				};
+				var releaseHandlers = function( dialog ) {
+					dialog.removeListener( 'ok', onClose );
+					dialog.removeListener( 'cancel', onClose );
+				};
+				var bindToDialog = function( dialog ) {
+					dialog.on( 'ok', onClose );
+					dialog.on( 'cancel', onClose );
+				};
+
+				editor.execCommand( 'colordialog' );
+
+				if ( editor._.storedDialogs && editor._.storedDialogs.colordialog )
+					bindToDialog( editor._.storedDialogs.colordialog );
+				else {
+					CKEDITOR.on( 'dialogDefinition', function( e ) {
+						if ( e.data.name != 'colordialog' )
+							return;
+
+						var definition = e.data.definition;
+
+						e.removeListener();
+						definition.onLoad = CKEDITOR.tools.override( definition.onLoad,
+							function( orginal ) {
+								return function() {
+									bindToDialog( this );
+									definition.onLoad = orginal;
+									if ( typeof orginal == 'function' )
+										orginal.call( this );
+								};
+							} );
+					} );
+				}
+			};
 		}
 
 		// It doesn't add commands, buttons or dialogs, it doesn't do anything here
 	} //Init
 } );
 
+
+	// * @param {Function} setup Setup function which returns a value instead of setting it.
+	// * @returns {Function} A function to be used in dialog definition.
+	function setupCells( setup ) {
+		return function( cells ) {
+			var fieldValue;
+
+			// Compatibility for previous versions of cell dialog as well as the table dialog (array vs single element)
+			if (cells.getAttribute) {
+				fieldValue = setup( cells );
+			} else {
+				fieldValue = setup( cells[ 0 ] );
+
+				// If one of the cells would have a different value of the
+				// property, set the empty value for a field.
+				for ( var i = 1; i < cells.length; i++ ) {
+					if ( setup( cells[ i ] ) !== fieldValue ) {
+						fieldValue = null;
+						break;
+					}
+				}
+			}
+
+			// Setting meaningful or empty value only makes sense
+			// when setup returns some value. Otherwise, a *default* value
+			// is used for that field.
+			if ( typeof fieldValue != 'undefined' ) {
+				this.setValue( fieldValue );
+
+				// The only way to have an empty select value in Firefox is
+				// to set a negative selectedIndex.
+				if ( CKEDITOR.env.gecko && this.type == 'select' && !fieldValue )
+					this.getInputElement().$.selectedIndex = -1;
+			}
+		};
+	}
 
 // This is the real code of the plugin
 CKEDITOR.on( 'dialogDefinition', function( ev )
@@ -57,6 +145,7 @@ CKEDITOR.on( 'dialogDefinition', function( ev )
 		var dialogName = ev.data.name,
 			dialogDefinition = ev.data.definition,
 			editor = ev.editor,
+			colorDialog = editor.plugins.colordialog,
 			tabName = '';
 
 		// Check if it's one of the dialogs that we want to modify and note the proper tab name.
@@ -85,10 +174,10 @@ CKEDITOR.on( 'dialogDefinition', function( ev )
 				type : 'text',
 				label : lang.label,
 				id : 'background',
-				setup : function( selectedElement )
+				setup : setupCells(function( selectedElement )
 				{
-					this.setValue( selectedElement.getAttribute( 'background' ) );
-				},
+					return selectedElement.getAttribute( 'background' );
+				}),
 				commit : function( data, selectedElement )
 				{
 					var element = selectedElement || data,
@@ -132,9 +221,9 @@ CKEDITOR.on( 'dialogDefinition', function( ev )
 				[lang.right_center,'right center'],
 				[lang.right_bottom,'right bottom']
 			],
-			setup: function (selectedElement) {
-				this.setValue( selectedElement.getStyle('background-position') );
-			},
+			setup: setupCells(function (selectedElement) {
+				return selectedElement.getStyle('background-position');
+			}),
 			onChange: function() {
 				var stylesInput = this.getDialog().getContentElement('advanced', 'advStyles');
 
@@ -167,9 +256,9 @@ CKEDITOR.on( 'dialogDefinition', function( ev )
 				[ lang.repeatY, 'repeat-y' ],
 				[ lang.repeatNone, 'no-repeat' ]
 			],
-			setup: function (selectedElement) {
-				this.setValue( selectedElement.getStyle('background-repeat') );
-			},
+			setup: setupCells(function (selectedElement) {
+				return selectedElement.getStyle('background-repeat');
+			}),
 			onChange: function() {
 				var stylesInput = this.getDialog().getContentElement('advanced', 'advStyles');
 
@@ -190,18 +279,115 @@ CKEDITOR.on( 'dialogDefinition', function( ev )
 			}
 		};
 
+		// The background-color field
+		var backgroundColor = {
+			type: 'hbox',
+			padding: 0,
+			widths: ['30px', '20px'],
+			id: 'bgColor',
+			children: [
+				{
+					type: 'text',
+					id: 'backgroundColor',
+					label: lang.color,
+					'default': '',
+					setup: setupCells(function(element) {
+						var bgColorAttr = element.getAttribute('backgroundColor'),
+							bgColorStyle = element.getStyle('background-color');
+
+						return bgColorStyle || bgColorAttr;
+					}),
+					onChange: function() {
+						var stylesInput = this.getDialog().getContentElement('advanced', 'advStyles');
+
+						if (stylesInput) {
+							stylesInput.updateStyle('background-color', this.getValue());
+						}
+					},
+					commit: function(data, selectedElement) {
+						var element = selectedElement || data,
+							value = this.getValue();
+
+						if (value){
+							element.setStyle('background-color', value);
+						}else{
+							element.removeStyle('background-color');
+						}
+					}
+				},
+				{
+					type: 'button',
+					id: 'bgColorChoose',
+					label: lang.chooseColor,
+					onLoad: function() {
+						// Stick the element to the bottom (#5587)
+						this.getElement().getParent().setStyle('vertical-align', 'middle');
+					},
+					onClick: function(element) {
+						editor.getColorFromDialog(function(color) {
+							var stylesInput = this.getDialog().getContentElement('advanced', 'advStyles');
+							var input = this.getDialog().getContentElement('advanced', 'backgroundColor');
+							input.setValue(color);
+
+							if (stylesInput && color) {
+								stylesInput.updateStyle('background-color', color);
+							}
+
+							input.focus();
+						}, this);
+					}
+				}
+			]
+		};
+
+		// The Attachment select field
+		var backgroundAttachment = {
+			type: 'select',
+			label: lang.attachment,
+			id: 'backgroundAttachment',
+			items:
+					[
+						[lang.attachmentScroll, ''],
+						[lang.attachmentFixed, 'fixed'],
+						[lang.attachmentLocal, 'local']
+					],
+			setup: setupCells(function(selectedElement) {
+				return selectedElement.getStyle('background-attachment');
+			}),
+			onChange: function() {
+				var stylesInput = this.getDialog().getContentElement('advanced', 'advStyles');
+
+				if (stylesInput) {
+					stylesInput.updateStyle('background-attachment', this.getValue());
+				}
+			},
+			commit: function(data, selectedElement) {
+				var element = selectedElement || data,
+						value = this.getValue(),
+						oBackground = this.getDialog().getContentElement(tabName, "background"),
+						background = oBackground && oBackground.getValue();
+
+				if (value && background)
+					element.setStyle('background-attachment', value);
+				else
+					element.removeStyle('background-attachment'); // it doesn't really work for the table
+			}
+		};
+
 		// Enabled/disabled automatically in 4.1 by ACF
-		if ( dialogName == 'cellProperties' )
+		if (dialogName == 'cellProperties')
 		{
 			textInput.requiredContent = 'td[background];th[background]';
 			backgroundPosition.requiredContent = 'td[background];th[background]';
 			backgroundRepeat.requiredContent = 'td[background];th[background]';
+			backgroundColor.requiredContent = 'td[background];th[background]';
 		}
 		else
 		{
 			textInput.requiredContent = 'table[background]';
 			backgroundPosition.requiredContent = 'table[background]';
 			backgroundRepeat.requiredContent = 'table[background]';
+			backgroundColor.requiredContent = 'table[background]';
 		}
 
 		// Add the elements to the dialog
@@ -215,16 +401,21 @@ CKEDITOR.on( 'dialogDefinition', function( ev )
 				widths: ['', '100px'],
 				children: [backgroundPosition, backgroundRepeat ]
 			});
+			tab.add({
+				type: 'hbox',
+				widths: ['50%', '50%'],
+				children: [backgroundAttachment, backgroundColor]
+			});
 		}
 		else
 		{
 			// In the cell dialog add it as a single row
 			browseButton.style = 'display:inline-block;margin-top:10px;';
 			tab.add({
-					type : 'hbox',
-					widths: [ '', '100px'],
-					children : [ textInput, browseButton],
-					requiredContent : textInput.requiredContent
+				type : 'hbox',
+				widths: [ '', '100px'],
+				children : [ textInput, browseButton],
+				requiredContent : textInput.requiredContent
 			});
 			tab.add({
 				type: 'hbox',
@@ -242,6 +433,12 @@ CKEDITOR.on( 'dialogDefinition', function( ev )
 		label	: 'Background image',
 		position : 'Background position',
 		repeat: 'Background repeat',
+		color: 'Background Color',
+		chooseColor: 'Choose',
+		attachment: 'Background Attachment',
+		attachmentScroll: 'Scroll',
+		attachmentFixed: 'Fixed',
+		attachmentLocal: 'Local',
 		repeatBoth : 'Repeat',
 		repeatX : 'Horizontally',
 		repeatY : 'Verticaly',
@@ -261,19 +458,25 @@ CKEDITOR.on( 'dialogDefinition', function( ev )
 		label	: 'Imagen de fondo',
 		position : 'Posición del fondo',
 		repeat: 'Repetición del fondo',
-		repeatBoth : 'Repetir',
-		repeatX : 'Horizontalmente',
-		repeatY : 'Verticalmente',
-		repeatNone : 'Ninguno',
-		left_top : 'Izquierda arriba',
-		left_center :'Izquierda centro',
-		left_bottom : 'Izquierda abajo',
-		center_top : 'Centro arriba',
-		center_center : 'Centro centro',
-		center_bottom :'Centro abajo',
-		right_top : 'Derecha arriba',
-		right_center :' Derecha centro',
-		right_bottom : 'Derecha abajo'
+		color: 'Color de fondo',
+		chooseColor: 'Elegir',
+		attachment: 'Desplazamiento',
+		attachmentScroll: 'Scroll',
+		attachmentFixed: 'Fixed',
+		attachmentLocal: 'Local',
+		repeatBoth: 'Repetir',
+		repeatX: 'Horizontalmente',
+		repeatY: 'Verticalmente',
+		repeatNone: 'Ninguno',
+		left_top: 'Izquierda arriba',
+		left_center: 'Izquierda centro',
+		left_bottom: 'Izquierda abajo',
+		center_top: 'Centro arriba',
+		center_center: 'Centro centro',
+		center_bottom: 'Centro abajo',
+		right_top: 'Derecha arriba',
+		right_center: ' Derecha centro',
+		right_bottom: 'Derecha abajo'
 	};
     var textsNl = {
         label	: 'Achtergrond afbeelding',
